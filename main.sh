447e58d106aa858f8e06d373209363c7568b3871
PrLeung <302548103@qq.com>
2025-01-30 20:22:37 +0800

    统一all_txt_supervisions 批量处理文本

---
+++ main.sh
@@ -0,0 +1,20 @@
+export http_proxy=http://127.0.0.1:7890;export https_proxy=http://127.0.0.1:7890
+python eval_AET.py --config ./configs/Retrieval_coco.yaml \
+	--cuda_id 6 \
+	--model_list ALBEF TCL CLIP_ViT CLIP_CNN \
+	--source_model CLIP_CNN \
+	--albef_ckpt ./checkpoints/albef_coco.pth \
+	--tcl_ckpt ./checkpoints/tcl_coco.pth \
+	--original_rank_index_path ./std_eval_idx/coco/\
+	# --batch_size 1
+
+python eval_AET.py --config ./configs/Retrieval_coco.yaml \
+	--cuda_id 6 \
+	--model_list ALBEF TCL CLIP_ViT CLIP_CNN \
+	--source_model ALBEF \
+	--albef_ckpt ./checkpoints/albef_coco.pth \
+	--tcl_ckpt ./checkpoints/tcl_coco.pth \
+	--original_rank_index_path ./std_eval_idx/coco/\
+	# --batch_size 1
+
+
